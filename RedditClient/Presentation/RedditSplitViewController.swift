c1d649c4ed7bf90bc8d9b7256f3ed3c83329b9e2
Manu Herrera <manuherrera8@gmail.com>
2018-12-02 05:04:14 -0300

    Add app wireframe (Split VC, Table VC, and Detail VC)

---
+++ RedditClient/Presentation/RedditSplitViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RedditSplitViewController.swift
+//  RedditClient
+//
+//  Created by Manu Herrera on 02/12/2018.
+//  Copyright © 2018 Manu Herrera. All rights reserved.
+//
+
+import UIKit
+
+class RedditSplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
