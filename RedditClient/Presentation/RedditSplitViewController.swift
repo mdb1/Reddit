b965f880b78a0177b3efb7c5e40bd8b4ded7a9a2
Manu Herrera <manuherrera8@gmail.com>
2018-12-02 09:05:32 -0300

    Final changes

--- RedditClient/Presentation/RedditSplitViewController.swift
+++ RedditClient/Presentation/RedditSplitViewController.swift
@@ -15,6 +15,18 @@ class RedditSplitViewController: UISplitViewController, UISplitViewControllerDel
     override func viewDidLoad() {
         super.viewDidLoad()
         let _ = self.vc.view
+        self.delegate = self
+        preferredDisplayMode = .allVisible
+    }
+    
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? RedditDetailViewController else { return false }
+        if topAsDetailController.view.backgroundColor != .red {
+            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+            return true
+        }
+        return false
     }
 
 }
